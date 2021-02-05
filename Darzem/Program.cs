517234c878b4b2d24feb8d30a35d7a48abe8e2b3
Гойман Антон Андреевич <gr691_gaa@ttit.local>
2021-02-05 10:48:23 +0700

    Добавьте файлы проекта.

---
+++ Darzem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pflfxf_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите число n");
+          int n=Convert.ToInt32(Console.ReadLine());
+           
+
+            Console.WriteLine("1"+n+"1");
+            Console.ReadLine();
+
+        }
+    }
+}
